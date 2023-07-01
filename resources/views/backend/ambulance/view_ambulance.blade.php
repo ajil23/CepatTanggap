875aba9075c445b1bdc6fe57e51be1dab244167d
ernakha <ernakhasanah78@gmail.com>
2023-07-01 20:15:13 +0700

    table-UI

--- resources/views/backend/ambulance/view_ambulance.blade.php
+++ resources/views/backend/ambulance/view_ambulance.blade.php
@@ -55,6 +55,108 @@
             </div>
             <!-- End of Main Content -->
 
+            <!-- Begin Page Content -->
+            <div class="container-fluid">
+
+                <!-- DataTales Example -->
+                <div class="card shadow mb-4">
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                <thead>
+                                    <tr>
+                                        <th>No</th>
+                                        <th>Nama Pasien</th>
+                                        <th>Asal Ambulance</th>
+                                        <th>Keperluan</th>
+                                        <th>Lokasi</th>
+                                        <th>Tanggal</th>
+                                        <th>Lihat Peta</th>
+                                        <th>Status</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>Indah Burhanuddin</td>
+                                        <td>Balai Desa Sukorejo</td>
+                                        <td>Persalinan</td>
+                                        <td>Sukorejo</td>
+                                        <td>1 Juli 2023</td>
+                                        <th>lihat</th>
+                                        <th></th>
+                                    </tr>
+                                    <tr>
+                                        <td>2</td>
+                                        <td>Usman Kusuma</td>
+                                        <td>Puskesmas Pedotan</td>
+                                        <td>Stroke</td>
+                                        <td>Ringintelu</td>
+                                        <td>1 Juli 2023</td>
+                                        <th>lihat</th>
+                                        <th></th>
+                                    </tr>
+                                    <tr>
+                                        <td>3</td>
+                                        <td>Kasih Eka</td>
+                                        <td>Puskesmas Kedungrejo</td>
+                                        <td>Kecelakaan</td>
+                                        <td>Kedungrejo</td>
+                                        <td>1 Juli 2023</td>
+                                        <th>lihat</th>
+                                        <th></th>
+                                    </tr>
+                                    <tr>
+                                        <td>4</td>
+                                        <td>Slamet Ali</td>
+                                        <td>Puskesmas Bangorejo</td>
+                                        <td>Usus Buntu</td>
+                                        <td>Bangorejo</td>
+                                        <td>1 Juli 2023</td>
+                                        <th>lihat</th>
+                                        <th></th>
+                                    </tr>
+                                    <tr>
+                                        <td>5</td>
+                                        <td>Hasan Firdaus</td>
+                                        <td>Balai Desa Pasembon</td>
+                                        <td>Sesak</td>
+                                        <td>Pasembon</td>
+                                        <td>1 Juli 2023</td>
+                                        <th>lihat</th>
+                                        <th></th>
+                                    </tr>
+                                    <tr>
+                                        <td>6</td>
+                                        <td>Mawar Wangi</td>
+                                        <td>Balai Desa Sukorejo</td>
+                                        <td>Gerd</td>
+                                        <td>Sukorejo</td>
+                                        <td>1 Juli 2023</td>
+                                        <th>lihat</th>
+                                        <th></th>
+                                    </tr>
+                                    <tr>
+                                        <td>7</td>
+                                        <td>Yuliana Mega</td>
+                                        <td>Balai Desa Sukorejo</td>
+                                        <td>Gigit Ular</td>
+                                        <td>Sukorejo</td>
+                                        <td>1 Juli 2023</td>
+                                        <th>lihat</th>
+                                        <th></th>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <!-- /.container-fluid -->
+
+
+
             @include('admin.body.footer')
 
 
