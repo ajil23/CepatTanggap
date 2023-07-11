81a8c68292e91550b87c2da3ebaa619d98ef31e1
ernakha <ernakhasanah78@gmail.com>
2023-07-11 14:15:36 +0700

    dashboard

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -13,23 +13,6 @@
        <!-- Content Row -->
        <div class="row">
 
-           <!-- Earnings (Monthly) Card Example -->
-           <div class="col-xl-3 col-md-6 mb-4">
-               <div class="card border-left-primary shadow h-100 py-2">
-                   <div class="card-body">
-                       <div class="row no-gutters align-items-center">
-                           <div class="col mr-2">
-                               <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                   Earnings (Monthly)</div>
-                               <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
-                           </div>
-                           <div class="col-auto">
-                               <i class="fas fa-calendar fa-2x text-gray-300"></i>
-                           </div>
-                       </div>
-                   </div>
-               </div>
-           </div>
 
            <!-- Earnings (Monthly) Card Example -->
            <div class="col-xl-3 col-md-6 mb-4">
@@ -38,11 +21,23 @@
                        <div class="row no-gutters align-items-center">
                            <div class="col mr-2">
                                <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                   Earnings (Annual)</div>
-                               <div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
+                                   Panggil Ambulance</div>
+                               <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                   <?php
+                                    $connection = mysqli_connect("localhost", "root", "", "ctdb");
+
+                                    $query = "SELECT id FROM pambulan ORDER BY id";
+                                    $query_run = mysqli_query($connection, $query);
+
+                                    $row = mysqli_num_rows($query_run);
+
+                                    echo "<h1> $row </h1>";
+                                    ?>
+
+                               </div>
                            </div>
                            <div class="col-auto">
-                               <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                               <i class="fas fa fa-ambulance fa-2x text-gray-300"></i>
                            </div>
                        </div>
                    </div>
@@ -55,21 +50,32 @@
                    <div class="card-body">
                        <div class="row no-gutters align-items-center">
                            <div class="col mr-2">
-                               <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Tasks
-                               </div>
+                               <div class="text-xs font-weight-bold text-info text-uppercase mb-1">
+                                   Panggil Bantuan</div>
                                <div class="row no-gutters align-items-center">
                                    <div class="col-auto">
-                                       <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
+                                       <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">
+                                           <?php
+                                            $connection = mysqli_connect("localhost", "root", "", "ctdb");
+
+                                            $query = "SELECT id FROM panakes ORDER BY id";
+                                            $query_run = mysqli_query($connection, $query);
+
+                                            $row = mysqli_num_rows($query_run);
+
+                                            echo "<h1> $row </h1>";
+                                            ?>
+                                       </div>
                                    </div>
-                                   <div class="col">
+                                   <!-- <div class="col">
                                        <div class="progress progress-sm mr-2">
                                            <div class="progress-bar bg-info" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
                                        </div>
-                                   </div>
+                                   </div> -->
                                </div>
                            </div>
                            <div class="col-auto">
-                               <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                               <i class="fas fa fa-user-md fa-2x text-gray-300"></i>
                            </div>
                        </div>
                    </div>
@@ -83,17 +89,61 @@
                        <div class="row no-gutters align-items-center">
                            <div class="col mr-2">
                                <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                                   Pending Requests</div>
-                               <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
+                                   Total Pasien</div>
+                               <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                   <?php
+                                    $connection = mysqli_connect("localhost", "root", "", "ctdb");
+
+                                    $query = "SELECT id FROM papasiens ORDER BY id";
+                                    $query_run = mysqli_query($connection, $query);
+
+                                    $row = mysqli_num_rows($query_run);
+
+                                    echo "<h1> $row </h1>";
+                                    ?>
+                               </div>
                            </div>
                            <div class="col-auto">
-                               <i class="fas fa-comments fa-2x text-gray-300"></i>
+                               <i class="fas fa fa-server fa-2x text-gray-300"></i>
                            </div>
                        </div>
                    </div>
                </div>
            </div>
+
+           
+       <!-- Earnings (Monthly) Card Example -->
+       <div class="col-xl-3 col-md-6 mb-4">
+           <div class="card border-left-primary shadow h-100 py-2">
+               <div class="card-body">
+                   <div class="row no-gutters align-items-center">
+                       <div class="col mr-2">
+                           <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                               Jumlah Petugas</div>
+                           <div class="h5 mb-0 font-weight-bold text-gray-800">
+                               <?php
+                                $connection = mysqli_connect("localhost", "root", "", "ctdb");
+
+                                $query = "SELECT id FROM users where role=1 ORDER BY id ";
+                                $query_run = mysqli_query($connection, $query);
+
+                                $row = mysqli_num_rows($query_run);
+
+                                echo "<h1> $row </h1>";
+                                ?>
+                           </div>
+                       </div>
+                       <div class="col-auto">
+                           <i class="fas fa-fw fa-tachometer-alt fa-2x text-gray-300"></i>
+                       </div>
+                   </div>
+               </div>
+           </div>
        </div>
+       </div>
+
+
+
 
        <!-- Content Row -->
 
